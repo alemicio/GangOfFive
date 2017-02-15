3d0d2a3d733b5ff463d4c6204cb94dedd9f1005f
SimoneGuasconi <simone91gv@gmail.com>
2017-02-15 12:51:23 +0100

    Added simple User constructor

--- src/main/java/it/ariadne/gangoffive/model/User.java
+++ src/main/java/it/ariadne/gangoffive/model/User.java
@@ -9,6 +9,14 @@ public class User implements Serializable {
 	private String username;
 	private String password;
 	
+	
+	
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
